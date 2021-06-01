5576a64de463423b8aca7b95f76c149cdef75d49
Jauav <85149411+Jauav@users.noreply.github.com>
2021-06-01 13:24:39 +0800

    Add files via upload

---
+++ app/src/main/java/com/twilio/conversationsquickstart/MainActivity2.java
@@ -0,0 +1,22 @@
+package com.twilio.conversationsquickstart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.twilio.conversationsquickstart.Policy;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Button;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
+
+
